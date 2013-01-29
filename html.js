1bafae672fdab08d583a5527070820be2e36083b
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-01-29 10:26:35 +0100

    Forgot core version reference updates. Damn\! This is becomming annoying and needs a solution\!

--- html.js
+++ html.js
@@ -1,4 +1,4 @@
-define(["github/adioo/bind/v0.2.0/bind", "github/adioo/events/v0.1.0/events", "/jquery.js"], function(Bind, Events) {
+define(["github/adioo/bind/v0.2.1/bind", "github/adioo/events/v0.1.2/events", "/jquery.js"], function(Bind, Events) {
 
     function Html(module) {
 
